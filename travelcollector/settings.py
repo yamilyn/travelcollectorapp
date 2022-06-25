bf36d7a6486771152611ca02621bd0eae75d14f8
Yamilyn <yamilysousa@hotmail.com>
2022-06-25 11:26:14 +0100

    deployment

--- travelcollector/settings.py
+++ travelcollector/settings.py
@@ -16,6 +16,8 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -127,7 +129,12 @@ LOGIN_REDIRECT_URL = '/travels/'
 
 LOGOUT_REDIRECT_URL = '/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
